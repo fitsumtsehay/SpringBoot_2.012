70f1ed4d08cbd9b054cc2a28053579fea2e8a8f9
fitsumtsehay <tsehayfitsum@gmail.com>
2020-05-19 03:20:31 -0400

    SpringBoot_2.012

---
+++ src/main/java/com/example/demo/SpringBoot_201_Pt2.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot_201_Pt2 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot_201_Pt2.class, args);
+    }
+}
